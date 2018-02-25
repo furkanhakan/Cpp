a08bd2397254685ea2d81c335936e70eddaa0d72
Furkan Hakan <34661221+furkanhakan@users.noreply.github.com>
2018-02-25 17:02:04 +0300

    Update KutuphaneOtomasyonu.cpp

--- KutuphaneOtomasyonu.cpp
+++ KutuphaneOtomasyonu.cpp
@@ -1,4 +1,4 @@
-// dosyalamaislemleri3.cpp : Defines the entry point for the console application.
+// KutuphaneOtomasyonuV1.0.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
@@ -37,12 +37,10 @@ void Kitap::KitapbilgileriGir()
 
 void Kitap::KitapBigileriYazdir()
 {
-	cout<<"Kitap Kodu....:"<<kitapkodu<<endl;
-	cout<<"Kitap Adı....:"<<kitapadi<<endl;
+	cout<<"Kitap Kodu......:"<<kitapkodu<<endl;
+	cout<<"Kitap Adı.......:"<<kitapadi<<endl;
 	cout<<"Kitap Yazarı....:"<<kitapyazariadi<<" "<<kitapyazarisoyadi<<endl;
 }
-
-
 
 class KitapKontrol
 {
@@ -57,7 +55,7 @@ class Kutuphane
 public:
 	void KitapEkle();
 	void KitapBul(int no);
-
+	void KitapSil(int no);
 
 };
 
@@ -109,30 +107,126 @@ void Kutuphane::KitapBul(int no)
 	kitapkayit.close();
 }
 
+void KitaplariListele()
+{
+	fstream kitapkayit;
+	Kitap kitap;
+	kitapkayit.open("Kayit.txt",ios::out|ios::in|ios::app);
+	cout<<"Kitap Kodu"<<"      "<<"Kitap Adı"<<endl;
+	cout<<"----------"<<"      "<<"---------"<<endl;
+	while (kitapkayit>>kitap.kitapkodu>>kitap.kitapadi>>kitap.kitapyazariadi>>kitap.kitapyazarisoyadi)
+	{
+		cout<<"     "<<kitap.kitapkodu<<"          "<<kitap.kitapadi<<endl;
+	}
+	kitapkayit.close();
+}
+
+void Kutuphane::KitapSil(int no)
+{
+	fstream kitapsil;
+	fstream yenidosya;
+	Kitap kitap;
+	kitapsil.open("Kayit.txt",ios::out|ios::in|ios::app);
+	yenidosya.open("Kayit1.txt",ios::out|ios::in|ios::app);
+	while (kitapsil>>kitap.kitapkodu>>kitap.kitapadi>>kitap.kitapyazariadi>>kitap.kitapyazarisoyadi)
+	{
+		if (no!=kitap.kitapkodu)
+		{
+			
+		yenidosya<<std::left<<setw(10)<<kitap.kitapkodu
+		<<setw(20)<<kitap.kitapadi<<
+		setw(15)<<kitap.kitapyazariadi<<
+		setw(10)<<kitap.kitapyazarisoyadi<<endl;
+		}
+		else
+		{
+			sayac++;
+		}
+	}
+	kitapsil.close();
+	 remove("Kayit.txt");
+	 yenidosya.close();
+	 rename("Kayit1.txt", "Kayit.txt");
+	 
+}
+
 void AnaMenu()
 {
 	Kutuphane islemler;
 	int secim;
 	int no;
-	cout.setf(ios::right);
-	cout<<std::right<<setw(15)<<"Düzce Üniversitesi Kütüphane Otomasyonu"<<endl;
-	cout<<"1-Kitap Ekle"<<endl;
-	cout<<"2-Kitap Ara"<<endl;
-	cout<<"3-Çıkış"<<endl;
+	int deletekitap;
+	
+	cout<<std::left<<setw(15)<<"                                      DÜZCE ÜNİVERSİTE KÜTÜPHANE OTOMASYONU"<<endl;
+	cout<<"1-) KİTAP EKLE "<<endl;
+	cout<<"2-) KİTAP ARA "<<endl;
+	cout<<"3-) KİTAP SİL "<<endl;
+	cout<<"9-) ÇIKIŞ "<<endl;
+	cout<<"SEÇİMİNİZ.....:";
 	cin>>secim;
 	switch (secim)
 	{
 	case 1:kitapkontrol=0; islemler.KitapEkle(); 	break;
 	case 2:	sayac=0;
-		cout<<"Bulmak İstediğiniz Kitap Kodunu Giriniz"; cin>>no; islemler.KitapBul(no);
+		system("cls");
+		cout<<std::left<<setw(15)<<"                                      DÜZCE ÜNİVERSİTE KÜTÜPHANE OTOMASYONU"<<endl;
+		KitaplariListele();
+		cout<<"Bulmak İstediğiniz Kitap Kodunu Giriniz....:"; cin>>no; islemler.KitapBul(no);
 		if (sayac==0)
 		{
 			cout<<"Kitap Bulunamadı";
-			system("pause");
+			int islem=0;
+			cout<<endl<<"0-Ana Menü"<<endl<<"9-Çıkış"<<endl;
+		cin>>islem;
+		if (islem==0)
+		{
+			system("cls");
+			AnaMenu();
+		}
+		else if (islem==9)
+		{
+			exit(0);
+		}
+		else
+		{
+			cout<<"Yanlış Seçim Yaptınız";
+		}
 			system("cls");
 		AnaMenu();
 		}		break;	
-	case 3:exit(0); break;
+	case 3:
+		sayac=0;
+		system("cls");
+		cout<<std::left<<setw(15)<<"                                      DÜZCE ÜNİVERSİTE KÜTÜPHANE OTOMASYONU"<<endl;
+		KitaplariListele();
+		cout<<"Silmek İstediğiniz Kitap Kodunu Giriniz....:"; cin>>deletekitap; islemler.KitapSil(deletekitap);
+
+		if (sayac==0)
+		{
+			cout<<"Kitap Bulunamadı";
+			int islem=0;
+			cout<<endl<<"0-Ana Menü"<<endl<<"9-Çıkış"<<endl;
+		cin>>islem;
+		if (islem==0)
+		{
+			system("cls");
+			AnaMenu();
+		}
+		else if (islem==9)
+		{
+			exit(0);
+		}
+		else
+		{
+			cout<<"Yanlış Seçim Yaptınız";
+		}
+			system("cls");
+		AnaMenu();
+		}		break;	
+
+
+
+	case 9:exit(0); break;
 	default:cout<<"Yanlış Seçim Yaptınız"; system("cls"); AnaMenu();
 		break;
 	}
@@ -146,14 +240,14 @@ int _tmain(int argc, _TCHAR* argv[])
 	int islem=0;
 	do
 	{
-		cout<<"0-Ana Menü"<<endl<<"3-Çıkış"<<endl;
+		cout<<"0-Ana Menü"<<endl<<"9-Çıkış"<<endl;
 		cin>>islem;
 		if (islem==0)
 		{
 			system("cls");
 			AnaMenu();
 		}
-		else if (islem==3)
+		else if (islem==9)
 		{
 			exit(0);
 		}
